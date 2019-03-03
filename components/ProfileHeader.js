9f2eeec0327bfcdee70e23925aaee1b0057b1aa6
Cameron Miller <cameronmiller@s-164-67-99-143.resnet.ucla.edu>
2019-03-03 03:37:05 -0800

    Improvements to Profile Page

--- components/ProfileHeader.js
+++ components/ProfileHeader.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {withNavigation} from 'react-navigation';
-import {View, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet, Button, Text} from 'react-native';
 
 class ProfileHeader extends React.Component {
 
     render() {
         return(
             <View style={styles.header}>
-                <Button title='Go Back' onPress={() => {
-                    this.props.navigation.goBack();
-                }}/>
+                <View style={styles.spacer}>
+                    <Button title='Go Back' onPress={() => {
+                        this.props.navigation.goBack();
+                    }}/>
+                </View>
+                <Text style={styles.headerText}>Profile</Text>
+                <View style={styles.spacer}/>
             </View>
         )
     }
@@ -21,8 +25,23 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         height: 40,
         borderBottomColor: 'lightgray',
-        borderBottomWidth: 1
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
         //TODO: header styling
+    },
+    
+    headerText: {
+        flex: 1,
+        fontSize: 24,
+        textAlign: 'center'
+    },
+
+    spacer: {
+        flex: 1,
+        height: 36,
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 
 });
--- pages/ProfilePage.js
+++ pages/ProfilePage.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, PixelRatio} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, PixelRatio, Dimensions, Alert} from 'react-native';
 import ProfileHeader from '../components/ProfileHeader';
 import PhotoUpload from 'react-native-photo-upload';
-import {ListItem} from 'react-native-elements';
+import {ListItem, Overlay, Button, Input} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 class ProfileScreen extends React.Component {
+    state = {
+      isVisible: false,
+      edit: 'Name'
+    }
+
     static navigationOptions = {
         header: () => {
             return(
             <ProfileHeader />
             )
         }
+    }
+
+    getPlaceholderText(input){
+      console.log(input);
+      if(input === "Name"){
+        return "Full Name";
+      }else if(input === "Age"){
+        return "Age";
+      }else if (input === "Height"){
+        return "Height"
+      }else if (input == "Weight"){
+        return "Weight"
+      }
     }
 
     render() {
@@ -40,18 +61,59 @@ class ProfileScreen extends React.Component {
             </PhotoUpload>
           </View>
           <View style={styles.body}>
+
+            <Overlay
+              isVisible={this.state.isVisible}
+              windowBackgroundColor="rgba(255, 255, 255, .5)"
+              overlayBackgroundColor="lightgray"
+              width= {screenWidth - 20}
+              height= {screenHeight/4}
+              overlayStyle={styles.overlay}
+            >
+              <Text style={styles.overlayText}>{this.state.edit}</Text>
+              
+              <Input
+                placeholder={this.getPlaceholderText(this.state.edit)}
+                leftIconContainerStyle={styles.overlayIcon}
+                onChangeText={(text) => {
+                  //TODO manage what to do with the text; set locally and input to database
+                }}
+                leftIcon={
+                  <Icon
+                    name='user'
+                    size={24}
+                    color='black'
+                  />
+                }
+              />
+
+              <Button 
+              title='Save'
+              style={styles.overlayButton}
+              onPress={() => {
+                this.setState({isVisible: false});
+              }}/>
+            </Overlay>
+
           {
             list.map((item, i) => (
-              <ListItem
-                Component= {TouchableOpacity}
-                key={i}
-                title={item.title}
-                leftIcon={{ name: item.icon }}
-                containerStyle = {styles.contentContainer}
-                onPress = {() => {
-                  //Set edit overlay to visible and get the correct info
-                }}
-              />
+              <View style={styles.listContainer}>
+                <ListItem
+                  Component= {TouchableOpacity}
+                  key={i}
+                  title={item.title}
+                  leftIcon={ {
+                    name: item.icon,
+                    size: 32
+                  }}
+                  containerStyle = {styles.contentContainer}
+                  onPress = {() => {
+                    this.setState({isVisible: true});
+                    this.setState({edit: item.title});
+                    console.log(item.title);
+                  }}
+                />
+              </View>
             ))
           }
         </View>
@@ -63,8 +125,24 @@ class ProfileScreen extends React.Component {
 const list = [
   {
     title: "Name",
-    icon: 'av-timer'
+    icon: "account-circle"
+    //material UI icon names
   },
+
+  {
+    title: "Age",
+    icon: "edit" //may find better ones in the future
+  },
+
+  {
+    title: "Height",
+    icon: "directions-run" //change this
+  },
+
+  {
+    title: "Weight",
+    icon: "timelapse"
+  }
 ]
 
 
@@ -74,8 +152,8 @@ const styles = StyleSheet.create({
     height:200,
   },
   avatar: {
-    width: 130,
-    height: 130,
+    width: 100,
+    height: 100,
     borderRadius: 63,
     borderWidth: 4,
     borderColor: "white",
@@ -126,7 +204,32 @@ const styles = StyleSheet.create({
   },
 
   contentContainer: {
-    backgroundColor: 'lightgray'
+    backgroundColor: 'lightgray',
+    borderRadius: 30
+  },
+
+  overlay: {
+    borderRadius: 10
+  },
+
+  overlayText: {
+    fontSize: 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    paddingVertical: 15
+  },
+
+  overlayIcon: {
+    padding: 10
+  },
+
+  overlayButton: {
+    padding: 20
+  },
+
+  listContainer: {
+    padding: 12
   }
 });
 
